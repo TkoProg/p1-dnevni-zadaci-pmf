aba2b1e795b4e17ae24ac38ecdf429a1d8cc1e01
tamiroladejo <koltam09@gmail.com>
2023-10-07 00:32:54 +0200

    Uradjen zadatak i PDF za peti dan.

---
+++ Sedmica 1/Zadatak 5.py
@@ -0,0 +1,8 @@
+print("Zastava Bosne i Hercegovine")
+linije = 9
+trice = 6
+for i in range(6):
+    print((linije+i)*"-", "x", "-", (trice-i)*"3", "--", sep="")
+
+# ovo je nacin koji prolly nisu ocekivali kad su dali ovaj zadatak ali mi je dosadno cratiti pomocu
+# metode tri navodnika
