094cd4084ed5a5ef98c3b90c4a432a5cda93972c
tamiroladejo <koltam09@gmail.com>
2023-10-06 16:42:30 +0200

    Preimenovao iz "prvi dan.py" u "Zadatak 1.py".

---
+++ Sedmica 1/Zadatak 2.py
@@ -0,0 +1,12 @@
+print("Tamir")
+print("Datum rodjenja")
+print("TKN")
+print("Odsjek za matematiku")
+print("Prirodno-matematicki fakultet")
+print("----------------------------------------------------")
+print("""* * *  * * *  *   *  *  * * *
+  *    *   *  * * *  *  *   *
+  *    * * *  *   *  *  * *
+  *    *   *  *   *  *  *  *
+  *    *   *  *   *  *  *   *
+""")
