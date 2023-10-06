e791ff3bcb736f77a6404ae0c08fc8fa17413675
tamiroladejo <koltam09@gmail.com>
2023-10-06 16:43:16 +0200

    Dodao crtu.

--- Sedmica 1/Zadatak 2.py
+++ Sedmica 1/Zadatak 2.py
@@ -3,7 +3,7 @@ print("Datum rodjenja")
 print("TKN")
 print("Odsjek za matematiku")
 print("Prirodno-matematicki fakultet")
-print("----------------------------------------------------")
+print("-----------------------------------------------------")
 print("""* * *  * * *  *   *  *  * * *
   *    *   *  * * *  *  *   *
   *    * * *  *   *  *  * *
