f2d93b18999e9fcb3897e34eb0ed3f775c441043
tamiroladejo <koltam09@gmail.com>
2023-10-09 20:08:36 +0200

    Sesti pdf i uradjen zadatak.

---
+++ Sedmica 2/Zadatak 6.py
@@ -0,0 +1,11 @@
+print("Unesite prvi cijeli broj:")
+x = int(input())
+print("Unesite drugi cijeli broj:")
+y = int(input())
+
+print("--------------------------")
+
+print(f"Zbir:      {x+y}")
+print(f"Razlika:   {x-y}")
+print(f"Proizvod:  {x*y}")
+print(f"Kolicnik:  {x/y}")
