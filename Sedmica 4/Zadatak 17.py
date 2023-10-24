8138aa05e52a2df591ae52e3bedc94f9b565123f
tamiroladejo <koltam09@gmail.com>
2023-10-24 14:31:28 +0200

    Sedamnaesti pdf i uradjen zadatak.

---
+++ Sedmica 4/Zadatak 17.py
@@ -0,0 +1,19 @@
+n = int(input("Unesite broj, program ispituje da li je broj super ili ne: "))
+
+start = n
+
+if n % 2 == 0 or n % 3 == 0:
+    while n > 1:
+        if n % 2 == 0:
+            n = n / 2
+        elif n % 3 == 0:
+            n = n / 3
+        else:
+            print("Broj nije super!")
+            break
+    if n == 1:
+        print("Broj je super!")
+elif n == 1:
+    print("Broj je super!")
+else:
+    print("Broj nije super!")
