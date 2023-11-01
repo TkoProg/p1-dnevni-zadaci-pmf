fe282b7f8ed8462af533badb649c286bdb69c310
tamiroladejo <koltam09@gmail.com>
2023-11-01 22:39:36 +0100

    Dvadeset treci pdf i uradjen zadatak.

---
+++ Sedmica 5/Zadatak 23.py
@@ -0,0 +1,8 @@
+brojevi = ["nula", "jedan", "dva", "tri", "cetiri", "pet", "sest", "sedam", "osam", "devet", "deset"]
+
+n = int(input("Unesite visecirfreni broj n: "))
+
+n = str(n)
+pp = n[-2]
+
+print(brojevi[int(pp)])
