10c21515feb4ae7ad4d09b5cb025a497f4da7940
tamiroladejo <koltam09@gmail.com>
2023-11-07 14:37:01 +0100

    Dvadeset sesti pdf i uradjen zadatak.

---
+++ Sedmica 6/Zadatak 26.py
@@ -0,0 +1,17 @@
+import math
+
+brojac = 0
+naj = 1
+
+while True:
+    n = int(input("Unesite prirodan broj: "))
+    if n == 1:
+        break
+    if brojac == 0:
+        naj = n
+        brojac += 1
+    naj = math.gcd(n, naj)
+
+print("--------------------------------------")
+
+print(f"NZD je {naj}.")
