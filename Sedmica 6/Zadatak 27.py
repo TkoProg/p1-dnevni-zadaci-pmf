959b4d01eadfb689ccf5ab1d4128577fc69cb12c
tamiroladejo <koltam09@gmail.com>
2023-11-07 15:09:33 +0100

    Dvadeset sedmi pdf i uradjen zadatak.

---
+++ Sedmica 6/Zadatak 27.py
@@ -0,0 +1,16 @@
+def fibonaci(x):
+    broj = 1
+    prethodni = 0
+    lista = []
+    for i in range(x):
+        lista.append(broj)
+        broj = prethodni + broj
+        prethodni = broj - prethodni
+    return lista
+
+
+n = int(input())
+
+niz = fibonaci(n)
+
+[print(x, end=" ") for x in niz]
