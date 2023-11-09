6410d34a2398919bab1a2f9f27ced4a67393f6fc
tamiroladejo <koltam09@gmail.com>
2023-11-09 23:29:07 +0100

    Dvadeset deveti pdf i uradjen zadatak.

--- Sedmica 6/Zadatak 29.py
+++ Sedmica 6/Zadatak 29.py
@@ -2,7 +2,7 @@ suma = 0
 brojac = 0
 
 while True:
-    n = input(f"Brojac {brojac+1}. broj: ")
+    n = input(f"Unesite {brojac+1}. broj: ")
     if n[0] == "3":
         suma += int(n)
         break
