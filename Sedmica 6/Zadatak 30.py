997992588db61a0f4ddc9aaca53b420fc0aaf1e3
tamiroladejo <koltam09@gmail.com>
2023-11-10 11:49:12 +0100

    Trideseti pdf i uradjen zadatak.

--- Sedmica 6/Zadatak 30.py
+++ Sedmica 6/Zadatak 30.py
@@ -1,3 +1,28 @@
 n = int(input("Unesite dimenzije kvadrata: "))
-a = int(input("unesite broj reda: "))
+a = int(input("Unesite broj reda: "))
 b = int(input("Unesite broj kolone: "))
+m = int(input("Unesite dimenzije unutrasnjeg kvadrata: "))
+
+matrica = []
+brojaca = 0
+brojacb = 0
+brojacm = 0
+
+for i in range(n):
+    red = []
+    for j in range(n):
+        red.append("-")
+    matrica.append(red)
+
+print(matrica)
+
+for i in range(a-1, m+a-1):
+    for j in range(b-1, m+b-1):
+        matrica[i][j] = " "
+
+print(matrica)
+
+for i in range(len(matrica)):
+    for j in range(len(matrica[i])):
+        print(matrica[i][j], end=" ")
+    print()
